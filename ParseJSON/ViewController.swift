02c98ed2e5520e57e7f0b4c5add7baf661f8283f
Tal <talspektor.gmail.com>
2020-11-17 15:18:06 +0200

    test

--- ParseJSON/ViewController.swift
+++ ParseJSON/ViewController.swift
@@ -8,11 +8,38 @@
 
 import UIKit
 
+struct WebsiteDescription: Decodable {
+    let name: String
+    let description: String
+    let courses: [Course]
+}
+
+struct Course: Decodable {
+    let id: Int
+    let name: String
+    let link: String
+    let imageUrl: String
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let stringUrl = "https://api.letsbuildthatapp.com/jsondecodable/website_description"
+        guard let url = URL(string: stringUrl) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard let data = data else { return }
+            
+            do {
+                let websiteDescription = try JSONDecoder().decode(WebsiteDescription.self, from: data)
+                print(websiteDescription)
+            } catch let jsonError {
+                print("Error serialize json", jsonError)
+            }
+        }.resume()
     }
 
 
